96a1abfb47113b570b727435729a519ea8994e82
v0dka-Developments <122687879+v0dka-Developments@users.noreply.github.com>
2023-05-10 20:42:45 +0100

    1231231
    123123

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Olmteka aa123123a',
+        title: 'Olmteka aa1231231231231a',
        
         items: [
           {

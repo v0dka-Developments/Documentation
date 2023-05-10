e9a9a97974d346b6978505fce1f0f7a6d83e0a40
v0dka-Developments <122687879+v0dka-Developments@users.noreply.github.com>
2023-05-10 20:00:48 +0100

    tes
    tes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Olmteka tset',
+        title: 'Olmteka tsetasdasd',
        
         items: [
           {

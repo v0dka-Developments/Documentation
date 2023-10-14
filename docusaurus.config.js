56667e55754f0ab23bc379f814cf98f7f84fdec2
v0dka-Developments <122687879+v0dka-Developments@users.noreply.github.com>
2023-10-14 04:14:47 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'http://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/test_docs/',
+  baseUrl: '/Documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'v0dka-developments', // Usually your GitHub org/user name.
-  projectName: 'test_docs', // Usually your repo name.
+  projectName: 'Documentation', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
